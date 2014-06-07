d6944fdacf0988b02f48e3321b678dc05c00cfb1
lucasw <lucasw_backwards@gmail.com>
2014-06-07 06:03:57 -0700

    Reading and displaying the council district json.  In the google map they appear as black outlined and transparent gray interiors - not too exciting but works very easily.

--- README.md
+++ README.md
@@ -2,3 +2,17 @@ gmap
 ====
 
 google maps api test
+
+
+Convert shp to geojson with no alteration
+=========================================
+
+Use ogr::
+  ogr2ogr -f "GeoJSON" seattle_city_council_districts.json seattle_city_council_districts.shp seattle_city_council_districts
+
+http://stackoverflow.com/questions/2223979/convert-a-shapefile-shp-to-xml-json
+
+Make a geojson from python
+==========================
+
+http://gis.stackexchange.com/a/41658
--- test1.js
+++ test1.js
@@ -1,10 +1,13 @@
+var map;
 function initialize() {
   var mapOptions = {
     center: new google.maps.LatLng(47.6043, -122.342),
         zoom: 8
   };
-  var map = new google.maps.Map(document.getElementById("map-canvas"),
+  map = new google.maps.Map(document.getElementById("map-canvas"),
       mapOptions);
+
+  map.data.loadGeoJson("../gis/data//seattle_city_council_districts/seattle_city_council_districts.json");
 }
 google.maps.event.addDomListener(window, 'load', initialize);
 
